8a56f031052020d16f6a08f565761078ec854086
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-04 11:03:25 -0300

    Criando entidade PUBLICAR.

---
+++ src/entities/Publicar.java
@@ -0,0 +1,63 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Publicar {
+
+	private Date momento;
+	private String titulo;
+	private String comentario;
+	private Integer likes;
+
+	private List<Comentario> comentarios = new ArrayList<>();
+
+	public Publicar(Date momento, String titulo, String comentario, Integer likes) {
+		this.momento = momento;
+		this.titulo = titulo;
+		this.comentario = comentario;
+		this.likes = likes;
+	}
+
+	public Publicar() {
+
+	}
+
+	public Date getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Date momento) {
+		this.momento = momento;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+}
